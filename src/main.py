b40f8348f54fa5a86d1a04f86f71742147d864d5
mak1non <me1orin@protonmail.com>
2020-09-22 23:50:12 +0900

    Make can "KeyboardInterrupt"

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import cv2
 import sys
 import line
 import wheel
@@ -18,24 +17,24 @@ def main():
     # モーター操作
     car = wheel.MortarHandler()
 
-    while True:
-        msg, detLB, detRB = line.detectLine()
+    try:
+        while True:
+            msg, detLB, detRB = line.detectLine()
 
-        if msg == '':
-            # 数値の表示
-            print("detLB : " + str(detLB) + " detRB: " + str(detRB))
+            if msg == '':
+                # 数値の表示
+                print("detLB : " + str(detLB) + " detRB: " + str(detRB))
 
-            # 線の判定
-            car.judgeLine(detLB, detRB)
-            print(car.direction)
-        else:
-            # エラー表示
-            print('エラー: ' + msg)
-            break
+                # 線の判定
+                car.judgeLine(detLB, detRB)
+                print(car.direction)
+            else:
+                # エラー表示
+                print('エラー: ' + msg)
+                break
 
-        # 終了
-        if cv2.waitKey(1) & 0xFF is ord('q'):
-            break
+    except KeyboardInterrupt:
+        cam.dispose()
 
 
 if __name__ == '__main__':
