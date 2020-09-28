ea4b3f94f77d644a22af5fa3b4734447cf0beeb5
mak1non <me1orin@protonmail.com>
2020-09-28 10:52:36 +0900

    Make config editable

--- src/line.py
+++ src/line.py
@@ -3,22 +3,6 @@ import datetime
 from config import Config
 from state import State
 
-# 二値化の閾値
-threshold = 64
-maxVal = 255
-
-# トリミングサイズ
-trimY = 180  # 縦座標
-trimH = 30   # 高さ
-
-# 左ブロックエリア設定
-leftXArea = (200, 210)  # 始点, 終点
-leftYArea = (0, trimH)
-
-# 右ブロックエリア設定
-rightXArea = (400, 410)
-rightYArea = (0, trimH)
-
 
 class Line:
 
@@ -48,7 +32,9 @@ class Line:
         # 画像を取得
         ret, self.origImg = self.camera.read()
         if ret is True:
-            trimImg = self.origImg[trimY:trimY + trimH, ]  # 画像をトリミング
+            # 画像をトリミング
+            trimImg = self.origImg[self.cfg.trimY:self.cfg.trimY +
+                                   self.cfg.trimH, ]
             grayImg = cv2.cvtColor(
                 trimImg, cv2.COLOR_BGR2GRAY)  # グレースケール化
         else:
@@ -57,14 +43,14 @@ class Line:
 
         # 画像の2値化
         ret, self.binaryImg = cv2.threshold(
-            grayImg, threshold, maxVal, cv2.THRESH_BINARY_INV)
+            grayImg, self.cfg.threshold, self.cfg.maxValue, cv2.THRESH_BINARY_INV)
         if ret:
             # 左ブロックエリアのフレームをセット
-            leftBlock = self.binaryImg[leftYArea[0]:leftYArea[1],
-                                       leftXArea[0]: leftXArea[1]]
+            leftBlock = self.binaryImg[0:self.cfg.trimH,
+                                       self.cfg.leftArea[0]:self.cfg.leftArea[1]]
             # 右ブロックエリアのフレームをセット
-            rightBlock = self.binaryImg[rightYArea[0]: rightYArea[1],
-                                        rightXArea[0]: rightXArea[1]]
+            rightBlock = self.binaryImg[0:self.cfg.trimH,
+                                        self.cfg.rightArea[0]:self.cfg.rightArea[1]]
 
             # 左ブロックエリアの白ピクセルカウント
             self.detLB = cv2.countNonZero(leftBlock)
@@ -85,14 +71,14 @@ class Line:
 
         # 左ブロックエリア描画
         cv2.rectangle(self.binaryImg,
-                      (leftXArea[0], leftYArea[0]),
-                      (leftXArea[1], leftYArea[1]),
+                      (self.cfg.leftArea[0], 0),
+                      (self.cfg.leftArea[1], self.cfg.trimH),
                       (0, 0, 255),
                       1)
         # 右ブロックエリア描画
         cv2.rectangle(self.binaryImg,
-                      (rightXArea[0], rightYArea[0]),
-                      (rightXArea[1], rightYArea[1]),
+                      (self.cfg.rightArea[0], 0),
+                      (self.cfg.rightArea[1], self.cfg.trimH),
                       (0, 0, 255),
                       1)
 
