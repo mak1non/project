adad03fa4bcc56bdbcd3f629c342bfed73383633
mak1non <me1orin@protonmail.com>
2020-10-29 11:06:09 +0900

    test code

---
+++ test/remote.py
@@ -0,0 +1,18 @@
+import serial
+
+def main():
+    ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
+
+    while True:
+        user_in = input('> ')
+
+        if user_in == 'q':
+            break
+
+        serial_out = bytes(user_in, 'utf-8')
+        ser.write(serial_out)
+    
+    ser.close()
+
+if __name__ == "__main__":
+    main()
