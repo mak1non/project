2fdb2c5fcd58235ac7459b31617e406c592e68b1
mak1non <me1orin@protonmail.com>
2020-11-20 19:46:12 +0900

    Fix test code

--- test/remote.py
+++ test/remote.py
@@ -1,30 +1,31 @@
 import serial
+import time
 
 
 def main():
-    arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
+    with serial.Serial(port='/dev/ttyACM0', baudrate=115200) as arduino:
+        time.sleep(1)
 
-    while True:
-        user_in = input('> ')
+        while True:
+            user_in = input('> ')
 
-        if user_in == 'q':
-            break
-        elif user_in == 's':
-            arduino.write(b'\x53')
-        elif user_in == 'a':
-            arduino.write(b'\x41')
-        elif user_in == 'b':
-            arduino.write(b'\x42')
-        elif user_in == 'l':
-            arduino.write(b'\x4c')
-        elif user_in == 'r':
-            arduino.write(b'\x52')
+            if user_in == 'q':
+                break
+            elif user_in == 's':
+                arduino.write(b'\x53')
+            elif user_in == 'a':
+                arduino.write(b'\x41')
+            elif user_in == 'b':
+                arduino.write(b'\x42')
+            elif user_in == 'l':
+                arduino.write(b'\x4c')
+            elif user_in == 'r':
+                arduino.write(b'\x52')
 
+            arduino.flush()
+
+        arduino.write(b'\x53')
         arduino.flush()
-
-    arduino.write(b'\x53')
-    arduino.flush()
-    arduino.close()
 
 
 if __name__ == "__main__":
